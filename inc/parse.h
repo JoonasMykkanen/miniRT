2e820ddd5008cb0fff7977ba9755d26b460f49d5
JoonasMykkanen <42795051+JoonasMykkanen@users.noreply.github.com>
2023-08-21 15:22:07 +0300

    removed t_data as not defined yet

--- inc/parse.h
+++ inc/parse.h
@@ -6,7 +6,7 @@
 /*   By: joonasmykkanen <joonasmykkanen@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/21 14:49:07 by joonasmykka       #+#    #+#             */
-/*   Updated: 2023/08/21 14:57:43 by joonasmykka      ###   ########.fr       */
+/*   Updated: 2023/08/21 15:21:54 by joonasmykka      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,5 @@
 # define PRASE_H
 
 # include "minirt.h"
-
-void	parse_input_file(t_data *data);
 
 #endif // !PARSE_H

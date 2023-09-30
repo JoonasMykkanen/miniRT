9959b8274a180d0d729a3eab0354391c756b82f8
JoonasMykkanen <42795051+JoonasMykkanen@users.noreply.github.com>
2023-09-30 08:17:49 +0300

    light direction now working but shadow cast is harsh, maybe we can smooth it out somehow but as of now I'll take it and move forward

--- src/render.c
+++ src/render.c
@@ -6,11 +6,49 @@
 /*   By: joonasmykkanen <joonasmykkanen@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 11:17:48 by jmykkane          #+#    #+#             */
-/*   Updated: 2023/09/30 07:13:58 by joonasmykka      ###   ########.fr       */
+/*   Updated: 2023/09/30 08:14:53 by joonasmykka      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
+
+int	calculate_color(t_data *data, t_vector point, t_color color)
+{
+	t_vector scaled_direction;
+    t_vector hit_pos;
+    t_vector norm;
+
+	// TODO --> previous line was using uninitialized direction vector
+	// for some reason that made the transition with facing light and "dark side"
+	// much smoother, why?
+	scaled_direction = vec_multis(data->scene.ray.dir, data->pix.closest_t);
+	hit_pos = subtract(data->scene.ray.orig, point);
+	hit_pos = vec_add(hit_pos, scaled_direction);
+	norm = normalize(hit_pos);
+	double d = fmax(dotProduct(norm, data->scene.light.position), 0.0);
+
+	double ambientR = data->scene.ambient.color.red * data->scene.ambient.intensity;
+	double ambientG = data->scene.ambient.color.green * data->scene.ambient.intensity;
+	double ambientB = data->scene.ambient.color.blue * data->scene.ambient.intensity;
+
+	ambientR *= color.red / 255.0;
+	ambientG *= color.green / 255.0;
+	ambientB *= color.blue / 255.0;
+
+	double spotR = (d * data->scene.light.brightness * data->scene.light.color.red) * color.red / 255.0;
+	double spotG = (d * data->scene.light.brightness * data->scene.light.color.green) *  color.green / 255.0;
+	double spotB = (d * data->scene.light.brightness * data->scene.light.color.blue) * color.blue / 255.0;
+
+	color.red = (int)(ambientR + spotR);
+	color.green = (int)(ambientG + spotG);
+	color.blue = (int)(ambientB + spotB);
+
+	if (color.red > 255) color.red = 255;
+	if (color.green > 255) color.green = 255;
+	if (color.blue > 255) color.blue = 255;
+	
+	return ft_color(color.red,color. green, color.blue, 0xff);
+}
 
 void draw_plane(t_data *data) 
 {
@@ -28,14 +66,13 @@ void draw_plane(t_data *data)
 		double d = dist(data->scene.ray.orig, intersection_point);
 		if (d > data->pix.closest_t && data->pix.closest_t != 10)
 			return ;
-		data->pix.color = ft_color(100, 100, 100, 0xff);
+		data->pix.color = calculate_color(data, data->scene.planes[0].point, data->scene.planes[0].color);
 	}
 }
 
 void	draw_sphere(t_data *data)
 {
-	t_vector scaled_direction;
-	t_vector ray_d;
+	t_vector scaled_direction;	
     t_vector hit_pos;
     t_vector norm;
     t_sphere aux;
@@ -53,35 +90,9 @@ void	draw_sphere(t_data *data)
 			a2 =idx;
 		}
 	}
-	if (t66 != 5000000000000.0 && t66 < data->pix.closest_t) {
+	if (t66 != DBL_MAX && t66 < data->pix.closest_t) {
 		data->pix.closest_t = t66;
-		scaled_direction = vec_multis(ray_d, t66);
-		hit_pos = subtract(data->scene.ray.orig, data->scene.spheres[a2].center);
-		hit_pos = vec_add(hit_pos, scaled_direction);
-		norm = normalize(hit_pos);
-		double d = fmax(dotProduct(norm, data->scene.light.position), 0.0);
-
-		double ambientR = data->scene.ambient.color.red * data->scene.ambient.intensity;
-		double ambientG = data->scene.ambient.color.green * data->scene.ambient.intensity;
-		double ambientB = data->scene.ambient.color.blue * data->scene.ambient.intensity;
-
-		ambientR *= data->scene.spheres[a2].color.red / 255.0;
-		ambientG *= data->scene.spheres[a2].color.green / 255.0;
-		ambientB *= data->scene.spheres[a2].color.blue / 255.0;
-
-		double spotR = (d * data->scene.light.brightness * data->scene.light.color.red) * data->scene.spheres[a2].color.red / 255.0;
-		double spotG = (d * data->scene.light.brightness * data->scene.light.color.green) *  data->scene.spheres[a2].color.green / 255.0;
-		double spotB = (d * data->scene.light.brightness * data->scene.light.color.blue) * data->scene.spheres[a2].color.blue / 255.0;
-
-		int red = (int)(ambientR + spotR);
-		int green = (int)(ambientG + spotG);
-		int blue = (int)(ambientB + spotB);
-
-		if (red > 255) red = 255;
-		if (green > 255) green = 255;
-		if (blue > 255) blue = 255;
-		
-		data->pix.color = ft_color(red, green, blue, 0xff);
+		data->pix.color = calculate_color(data, data->scene.spheres[a2].center, data->scene.spheres[a2].color);
 	} 
 }
 
